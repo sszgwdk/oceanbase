47a8901144498ddb999ae0d14ce176aa1a0242ac
sszgwdk <2664407884@qq.com>
2024-11-24 17:18:09 +0800

    try wk cache

--- deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
+++ deps/oblib/src/lib/vector/vsag_lib/src/simd/avx512.cpp
@@ -27,70 +27,71 @@ float
 L2SqrSIMD16ExtAVX512(const void* pVect1v, const void* pVect2v, const void* qty_ptr) {
     float* pVect1 = (float*)pVect1v;
     float* pVect2 = (float*)pVect2v;
-    // size_t qty = *((size_t*)qty_ptr);
-    float PORTABLE_ALIGN64 TmpRes[16];
-    
-    __m512 diff1, v11, v12, diff2, v21, v22, diff3, v31, v32, diff4, v41, v42, diff5, v51, v52, diff6, v61, v62, diff7, v71, v72, diff8, v81, v82;
-    __m512 sum = _mm512_set1_ps(0);
-    v11 = _mm512_loadu_ps(pVect1);
-    v12 = _mm512_loadu_ps(pVect2);
-    diff1 = _mm512_sub_ps(v11, v12);
-    sum = _mm512_fmadd_ps(diff1, diff1, sum);
-    v21 = _mm512_loadu_ps(pVect1 + 16);
-    v22 = _mm512_loadu_ps(pVect2 + 16);
-    diff2 = _mm512_sub_ps(v21, v22);
-    sum = _mm512_fmadd_ps(diff2, diff2, sum);
-    v31 = _mm512_loadu_ps(pVect1 + 32);
-    v32 = _mm512_loadu_ps(pVect2 + 32);
-    diff3 = _mm512_sub_ps(v31, v32);
-    sum = _mm512_fmadd_ps(diff3, diff3, sum);
-    v41 = _mm512_loadu_ps(pVect1 + 48);
-    v42 = _mm512_loadu_ps(pVect2 + 48);
-    diff4 = _mm512_sub_ps(v41, v42);
-    sum = _mm512_fmadd_ps(diff4, diff4, sum);
-    v51 = _mm512_loadu_ps(pVect1 + 64);
-    v52 = _mm512_loadu_ps(pVect2 + 64);
-    diff5 = _mm512_sub_ps(v51, v52);
-    sum = _mm512_fmadd_ps(diff5, diff5, sum);
-    v61 = _mm512_loadu_ps(pVect1 + 80);
-    v62 = _mm512_loadu_ps(pVect2 + 80);
-    diff6 = _mm512_sub_ps(v61, v62);
-    sum = _mm512_fmadd_ps(diff6, diff6, sum);
-    v71 = _mm512_loadu_ps(pVect1 + 96);
-    v72 = _mm512_loadu_ps(pVect2 + 96);
-    diff7 = _mm512_sub_ps(v71, v72);
-    sum = _mm512_fmadd_ps(diff7, diff7, sum);
-    v81 = _mm512_loadu_ps(pVect1 + 112);
-    v82 = _mm512_loadu_ps(pVect2 + 112);
-    diff8 = _mm512_sub_ps(v81, v82);
-    sum = _mm512_fmadd_ps(diff8, diff8, sum);
 
-    // 原来的代码
-    // size_t qty16 = qty >> 4;
+    __m512 sum1 = _mm512_setzero_ps();
+    __m512 sum2 = _mm512_setzero_ps();
+    __m512 sum3 = _mm512_setzero_ps();
+    __m512 sum4 = _mm512_setzero_ps();
+    __m512 sum5 = _mm512_setzero_ps();
+    __m512 sum6 = _mm512_setzero_ps();
+    __m512 sum7 = _mm512_setzero_ps();
+    __m512 sum8 = _mm512_setzero_ps();
 
-    // const float* pEnd1 = pVect1 + (qty16 << 4);
+    // Fully unroll the loop to process 128 floats in total
+    __m512 v11 = _mm512_load_ps(pVect1 + 0);
+    __m512 v12 = _mm512_load_ps(pVect2 + 0);
+    __m512 diff1 = _mm512_sub_ps(v11, v12);
+    sum1 = _mm512_fmadd_ps(diff1, diff1, sum1);
 
-    // __m512 diff, v1, v2;
-    // // whp 
-    // __m512 sum = _mm512_set1_ps(0);
+    __m512 v21 = _mm512_load_ps(pVect1 + 16);
+    __m512 v22 = _mm512_load_ps(pVect2 + 16);
+    __m512 diff2 = _mm512_sub_ps(v21, v22);
+    sum2 = _mm512_fmadd_ps(diff2, diff2, sum2);
 
-    // while (pVect1 < pEnd1) {
-    //     v1 = _mm512_loadu_ps(pVect1);
-    //     pVect1 += 16;
-    //     v2 = _mm512_loadu_ps(pVect2);
-    //     pVect2 += 16;
-    //     diff = _mm512_sub_ps(v1, v2);
-    //     // whp
-    //     sum = _mm512_fmadd_ps(diff, diff, sum);
-    //     // sum = _mm512_add_ps(sum, _mm512_mul_ps(diff, diff));
-    // }
+    __m512 v31 = _mm512_load_ps(pVect1 + 32);
+    __m512 v32 = _mm512_load_ps(pVect2 + 32);
+    __m512 diff3 = _mm512_sub_ps(v31, v32);
+    sum3 = _mm512_fmadd_ps(diff3, diff3, sum3);
 
-    _mm512_store_ps(TmpRes, sum);
-    float res = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] +
-                TmpRes[7] + TmpRes[8] + TmpRes[9] + TmpRes[10] + TmpRes[11] + TmpRes[12] +
-                TmpRes[13] + TmpRes[14] + TmpRes[15];
+    __m512 v41 = _mm512_load_ps(pVect1 + 48);
+    __m512 v42 = _mm512_load_ps(pVect2 + 48);
+    __m512 diff4 = _mm512_sub_ps(v41, v42);
+    sum4 = _mm512_fmadd_ps(diff4, diff4, sum4);
 
-    return (res);
+    __m512 v51 = _mm512_load_ps(pVect1 + 64);
+    __m512 v52 = _mm512_load_ps(pVect2 + 64);
+    __m512 diff5 = _mm512_sub_ps(v51, v52);
+    sum5 = _mm512_fmadd_ps(diff5, diff5, sum5);
+
+    __m512 v61 = _mm512_load_ps(pVect1 + 80);
+    __m512 v62 = _mm512_load_ps(pVect2 + 80);
+    __m512 diff6 = _mm512_sub_ps(v61, v62);
+    sum6 = _mm512_fmadd_ps(diff6, diff6, sum6);
+
+    __m512 v71 = _mm512_load_ps(pVect1 + 96);
+    __m512 v72 = _mm512_load_ps(pVect2 + 96);
+    __m512 diff7 = _mm512_sub_ps(v71, v72);
+    sum7 = _mm512_fmadd_ps(diff7, diff7, sum7);
+
+    __m512 v81 = _mm512_load_ps(pVect1 + 112);
+    __m512 v82 = _mm512_load_ps(pVect2 + 112);
+    __m512 diff8 = _mm512_sub_ps(v81, v82);
+    sum8 = _mm512_fmadd_ps(diff8, diff8, sum8);
+
+    // Horizontal add to get the final result
+    sum1 = _mm512_add_ps(sum1, sum2);
+    sum3 = _mm512_add_ps(sum3, sum4);
+    sum5 = _mm512_add_ps(sum5, sum6);
+    sum7 = _mm512_add_ps(sum7, sum8);
+
+    sum1 = _mm512_add_ps(sum1, sum3);
+    sum5 = _mm512_add_ps(sum5, sum7);
+
+    sum1 = _mm512_add_ps(sum1, sum5);
+
+    float res = _mm512_reduce_add_ps(sum1);
+
+    return res;
 }
 
 float
--- deps/oblib/src/lib/vector/vsag_lib/src/version.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/version.h
@@ -1,4 +1,4 @@
 #ifndef VSAG_VERSION_H_
 #define VSAG_VERSION_H_
-#define VSAG_VERSION "30a0998287"
+#define VSAG_VERSION "68d8faf787-dirty"
 #endif  /* VSAG_VERSION_H_ */
