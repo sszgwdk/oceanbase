e225c50c1bfb8d5b5f84926b8c9e500d633b97ed
sszgwdk <2664407884@qq.com>
2024-12-02 05:27:15 +0000

    fix cmakelist: link libsimd_test.a

--- deps/oblib/src/lib/vector/vsag_lib/src/version.h
+++ deps/oblib/src/lib/vector/vsag_lib/src/version.h
@@ -1,4 +1,4 @@
 #ifndef VSAG_VERSION_H_
 #define VSAG_VERSION_H_
-#define VSAG_VERSION "adc75be7df-dirty"
+#define VSAG_VERSION "v4.2.0_CE_BETA-8565-gb8385d4a6a-dirty"
 #endif  /* VSAG_VERSION_H_ */
--- update_code_restart.sh
+++ update_code_restart.sh
@@ -2,9 +2,9 @@
 
 # 重新编译 + 切换二进制文件
 # bash build.sh release --make -j2
-sleep 2
-cp ./build_release/deps/oblib/src/lib/vector/vsag_lib/src/simd/libsimd.a ./deps/3rd/usr/local/oceanbase/deps/devel/lib/vsag_lib/libsimd.a
-sleep 2
+# sleep 2
+# cp ./build_release/deps/oblib/src/lib/vector/vsag_lib/src/simd/libsimd.a ./deps/3rd/usr/local/oceanbase/deps/devel/lib/vsag_lib/libsimd.a
+# sleep 2
 bash build.sh release --make -j2
 sleep 2
 
